e4d6d85dbcf5f256db16eec77ea3f4a09a8f0715
Sau NgaiFu <610035976@qq.com>
2018-02-09 14:34:33 +0800

    Add files via upload

---
+++ TestPrint.java
@@ -0,0 +1,23 @@
+
+public class TestPrint {
+
+	public static void main(String[] args)
+	{
+		int num=34, max=30;
+		if ( num >= max*2 )
+		    System.out.println("zhang");
+		    System.out.println("huang");
+		System.out.println("zhu");
+		
+		int limit = 100;
+		int num1 = 15;
+		int num2 = 40;
+		if ( limit <= limit)
+		{
+		    if ( num1 == num2 )
+		        System.out.println("lemon");
+		    System.out.println("lime");
+		}
+		System.out.println("grape");            
+	}
+}
